d6fe8b60b8c61975e58d3dd82276aac58dfb8bcb
mkuriki1990 <mkuriki@mkurikiMBP.local>
2017-09-02 16:00:33 +0900

    first commit

---
+++ mkruby.rb
@@ -0,0 +1,17 @@
+﻿#!/usr/bin/ruby
+
+# ファイル読み込み
+templete = File.open('templete_B6.tex', 'r')
+
+source = File.open('m40.txt.ruby', 'r')
+
+buffer = templete.read();
+src = source.read();
+
+buffer.gsub!("LILYCS", src)
+p buffer
+
+fff = File.open('test.tex', 'w')
+
+fff.write(buffer)
+fff.close
