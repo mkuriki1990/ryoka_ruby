cbbd7e5ce94e70bdd654ecb99fd7e4b99157469d
mkuriki1990 <mkuriki1990@gmail.com>
2017-09-04 22:09:39 +0900

    txt ファイルを読み込んで TeX トークンを貼るスクリプトに変更

--- mkruby.rb
+++ mkruby.rb
@@ -1,17 +1,109 @@
 ﻿#!/usr/bin/ruby
+# 寮歌の歌詞 txt ファイルの漢字に TeX トークン 
+# \ruby{漢字}{} をつけるスクリプト
+require 'csv'
 
-# ファイル読み込み
-templete = File.open('templete_B6.tex', 'r')
+# 収録曲一覧の読み込み
+csvList = CSV.read('./src/ryoka_list.csv', headers:true)
+csvList.each{|data|
+    srcName = "#{data["ファイル名"]}".gsub!(/.*\//, '')
+    title = "#{data["曲名"]}"
+    # year = "#{data["年"]}"
+    year = "#{data[0]}" # 0 番目要素の "年" だけヘッダ名で取得できない
+    name1 = "#{data["作歌"]}"
+    name2 = "#{data["作曲"]}"
 
-source = File.open('m40.txt.ruby', 'r')
+    # src ディレクトリにある txt ファイルの読み込み
+    source = File.open("./src/#{srcName}", 'r')
+    src = source.read()
 
-buffer = templete.read();
-src = source.read();
+    # 歌詞のヘッダとフッタを除外する
+    lilycs = "" # 歌詞を入れる文字列変数
+    buffer = "" # 一行を一時保存する文字列変数
+    footer = "" # フッタを一時保存する文字列変数
+    isHeaderEnd = false # ヘッダ判断用のフラグ
+    isFooterBegin = false # ヘッダ判断用のフラグ
 
-buffer.gsub!("LILYCS", src)
-p buffer
+    src.lines{|line|
+        # 最初の空行まではヘッダとして除外
+        if !isHeaderEnd
+            if line == "\n"
+                isHeaderEnd = true
+            end
+            next
+        end
 
-fff = File.open('test.tex', 'w')
+        # ヘッダ以外で半角 '(' から始まる行からはフッタ
+        # フッタにルビはいらないので, 後から付け足すため保存
+        if line =~ /^\(/ && !isFooterBegin
+            # 括弧が見つかった直後は buffer を処理して footer に記録開始
+            isFooterBegin = true
+            lilycs << buffer.chomp
+            footer << line
+            next
+        end
+        if isFooterBegin
+            # フッタが始まってからは footer のみに追記
+            footer << line
+            next
+        end
 
-fff.write(buffer)
-fff.close
+        # 空行の直前に TeX の改行文字 "\\" を入れないための処理
+        if line == "\n"
+            lilycs << buffer.chomp << "\n"
+        else
+            lilycs << buffer.chomp << "\\\\\n"
+        end
+        buffer = line
+    }
+    if !isFooterBegin
+        lilycs << buffer.chomp # 最後の行を追記
+    end
+
+    lilycs.gsub!(/([一-龠々])/, '\\ruby{\1}{}') # 漢字全てを '\ruby{漢字}{}' で markup
+
+    # 連続する漢字をまとめる処理 ここから
+    lilycs.gsub!(/{}\\ruby{/, '') # '{}' と '\ruby' が連続するものを削除
+    lilycs.gsub!(/{}/, '#') # 残った '{}' を一旦 '#' に置き換え
+    lilycs.gsub!(/(.)}/, '\1') # 残された '}' を全部消す
+    lilycs.gsub!(/#/, '}{}') # '#' にした '{}' を '}{}' として元に戻す
+    # 連続する漢字をまとめる処理 ここまで
+
+    # 曲番を除いて TeX トークン \item と \vspace に変換
+    lilycs.gsub!(/.+\.\\\\\n/, '') # 任意の文字にドットがつくものは曲番なので覗く
+    lilycs.gsub!(/^\\\\$/, "\n\\vspace{\\linespace}\n\\item") # \\ だけの行を '\vspace{\linespace}\n\item' に置き換え
+
+    # フッタがあれば処理する
+    if footer.length != 0
+        # フッタの半角括弧を全角括弧に変換
+        footer.gsub!('(', '（').gsub!(')', '）')
+        # フッタを追記
+        lilycs = lilycs << footer
+    end
+
+    # srcName.ruby ファイルに書き出し
+    result = File.open("ruby/#{srcName}.ruby", 'w')
+    result.write(lilycs)
+    result.close
+}
+
+
+###################
+# ここから TeX ファイルの作成
+###################
+
+# TeX のテンプレートファイル読み込み
+template = File.open('document_B6.tex', 'r')
+
+
+# buffer = templete.read();
+# src = source.read();
+# 
+# buffer.gsub!("LILYCS", src)
+# p buffer
+# 
+# fff = File.open('test.tex', 'w')
+# 
+# fff.write(buffer)
+# fff.close
+
