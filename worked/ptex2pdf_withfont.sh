e079d5a34cedb535a112a6cb84020dfe0903ee17
mkuriki1990 <mkuriki1990@gmail.com>
2019-02-03 18:43:35 +0900

    フォントマップのパス修正

--- worked/ptex2pdf_withfont.sh
+++ worked/ptex2pdf_withfont.sh
@@ -2,6 +2,6 @@
 
 for files in $*
 do
-    ptex2pdf -l -od "-f ../uptex-kozuka-pr6n-04.map -f ../otf-kozuka-pr6n.map" $files
+    ptex2pdf -l -od "-f ../fontmap/uptex-kozuka-pr6n-04.map -f ../fontmap/otf-kozuka-pr6n.map" $files
 done
 
